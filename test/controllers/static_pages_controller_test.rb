021daa12c520ce3b603133871faff82fbdfe29a6
guillonapa <guillonapa@gmail.com>
2018-08-29 21:40:23 -0400

    set ruby version file and modified gemfile

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "getting the home page" do
+    get root_path
+    assert_response(:success)
+
+  end
 end
